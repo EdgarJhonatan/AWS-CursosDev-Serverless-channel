98128dcf25a9d991227b89f9f1a784b1d1059a47
Edgar Delgadillo <edgar.delgadillo@acceso.com.pe>
2021-10-02 13:06:24 -0500

    actualizando channel

--- src/functions/list/index.ts
+++ src/functions/list/index.ts
@@ -2,6 +2,7 @@ import { handlerPath } from "@libs/handlerResolver";
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
+  name: "${self:custom.channel.name}-${self:provider.stage}-${self:custom.lambda.type}-list-medics",
   layers: ["${ssm:/group01/${self:provider.stage}/layer}"],
   events: [
     {
